854bccf92089df15b5f1271927954c75af95df40
makifcevik <makif.cevik02@gmail.com>
2025-10-09 13:31:43 +0300

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+﻿// kv_store.cpp : Defines the entry point for the application.
+//
+
+#include "kv_store.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
